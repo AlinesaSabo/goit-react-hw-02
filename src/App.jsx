8d5dd7bcf69d988fa0489086db37ae20590da4c1
Alinesa <alinesa322@gmail.com>
2024-09-05 23:30:01 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
+import { useEffect, useState } from "react";
 import Description from "./components/Description/Description";
-import Feadback from "./components/Feadback/Feadback";
 import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
 const App = () => {
-  const titleButton = [
-    {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    },
-  ];
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
   return (
-    <div>
+    <div className="container">
       <Description />
-      <Options data={titleButton} />
-      <Feadback data={titleButton} />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification message="No feedback given yet" />
+      )}
     </div>
   );
 };
